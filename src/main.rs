2a4c0ab7ed151720a6f3ac3a820cb0b3296f6330
hunterdw0007 <hunterw.mail@gmail.com>
2023-01-21 22:12:06 -0600

    feat: error handling and code modularity for input args

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,25 @@
 // Hunter Westerlund
 // 01/20/23
-/*
-	This is my implementation of the Minigrep project
-*/
 
 use std::env;
+use std::process;
+
+use minigrep::Config;
 
 fn main() {
 	let args: Vec<String> = env::args().collect();
 
-	let query =     &args[1];
-	let file_path = &args[2];
+	let config = Config::build(&args).unwrap_or_else(|err| {
+		println!("Problem parsing arguments: {err}");
+		process::exit(1);
+	});
 
-	println!("Searching for {}", query);
-	println!("In file {}", file_path);
+	println!("Searching for {}", config.query);
+	println!("In file {}", config.file_path);
+
+	if let Err(e) = minigrep::run(config) {
+		println!("Application error: {e}");
+		process::exit(1);
+	}
 }
+
